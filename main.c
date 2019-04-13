8b67fd4f120add5afb0681f6fa6e3a7b2a51a1b8
Max Base <MaxBaseCode@Gmail.Com>
2019-04-13 22:27:50 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : ShowAli/main.c
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-04-13
+* @Released under : https://github.com/BaseMax/ShowAli//blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/ShowAli/
+*
+**/
